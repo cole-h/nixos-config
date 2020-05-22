250b73acb5d9f680d63ba39170fc069ee1fb5e3c
Cole Helbling <cole.e.helbling@outlook.com>
2020-05-22 08:31:27 -0700

    boot: change timeout to 1s, make params go first

--- nixops/scadrial/modules/boot.nix
+++ nixops/scadrial/modules/boot.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 1;
   boot.supportedFilesystems = [ "zfs" ];
   boot.initrd.kernelModules = [ "nouveau" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -15,7 +16,7 @@
     "kernel.sysrq" = 1;
   };
 
-  boot.kernelParams = [
+  boot.kernelParams = lib.mkBefore [
     "quiet"
     "udev.log_priority=3"
     "elevator=none" # recommended for ZFS: https://grahamc.com/blog/nixos-on-zfs
