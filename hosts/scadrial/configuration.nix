8aae7a63121e02638bf28b998f7e0e09f4d91fe2
Cole Helbling <cole.e.helbling@outlook.com>
2020-09-08 13:15:08 -0700

    scadrial: update
    Latest nixos-unstable (check flake.lock for "large") fixes emoji again,
    so I can uncomment my emoji fontconfig stuff :D

--- hosts/scadrial/configuration.nix
+++ hosts/scadrial/configuration.nix
@@ -9,94 +9,76 @@
     ./modules
   ];
 
-  # link directories doesn't work
-  # environment.persistence."/media".link.directories = [
-  #   "/asdf"
-  # ];
-
-  # bind files doesn't work
-  # environment.persistence."/media".bind.files = [
-  #   "/jkl"
-  # ];
-
-  # home-manager = {
-  #   users.vin = import ../../home.nix;
-  #   useGlobalPkgs = true;
-  #   useUserPackages = true;
-  #   verbose = true;
-  # };
-
   nix.package = lib.mkDefault pkgs.nixUnstable;
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
   # TODO: JP fonts and IME
-  # omg color emoji finally <-- fucking JK, after updating nixpkgs again, it all came undone.
-  # I fucking hate fontconfig
-  # fonts.fontconfig.localConf = ''
-  #   <?xml version="1.0"?>
-  #   <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-  #   <fontconfig>
-  #     <!-- Priority:
-  #          1. The generic family OR specific family
-  #          2. The emoji font family (defined in 60-generic.conf)
-  #          3. All the rest
-  #     -->
-  #     <alias binding="weak">
-  #       <family>monospace</family>
-  #       <prefer>
-  #         <family>emoji</family>
-  #       </prefer>
-  #     </alias>
-  #     <alias binding="weak">
-  #       <family>sans-serif</family>
-  #       <prefer>
-  #         <family>emoji</family>
-  #       </prefer>
-  #     </alias>
+  # omg color emoji finally jtojnar is the best
+  fonts.fontconfig.localConf = ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+    <fontconfig>
+      <!-- Priority:
+           1. The generic family OR specific family
+           2. The emoji font family (defined in 60-generic.conf)
+           3. All the rest
+      -->
+      <alias binding="weak">
+        <family>monospace</family>
+        <prefer>
+          <family>emoji</family>
+        </prefer>
+      </alias>
+      <alias binding="weak">
+        <family>sans-serif</family>
+        <prefer>
+          <family>emoji</family>
+        </prefer>
+      </alias>
 
-  #     <alias binding="weak">
-  #       <family>serif</family>
-  #       <prefer>
-  #         <family>emoji</family>
-  #       </prefer>
-  #     </alias>
+      <alias binding="weak">
+        <family>serif</family>
+        <prefer>
+          <family>emoji</family>
+        </prefer>
+      </alias>
 
-  #     <selectfont>
-  #       <rejectfont>
-  #         <!-- Reject DejaVu fonts, they interfere with color emoji. -->
-  #         <pattern>
-  #           <patelt name="family">
-  #             <string>DejaVu Sans</string>
-  #           </patelt>
-  #         </pattern>
-  #         <pattern>
-  #           <patelt name="family">
-  #             <string>DejaVu Serif</string>
-  #           </patelt>
-  #         </pattern>
-  #         <pattern>
-  #           <patelt name="family">
-  #             <string>DejaVu Sans Mono</string>
-  #           </patelt>
-  #         </pattern>
+      <selectfont>
+        <rejectfont>
+          <!-- Reject DejaVu fonts, they interfere with color emoji. -->
+          <pattern>
+            <patelt name="family">
+              <string>DejaVu Sans</string>
+            </patelt>
+          </pattern>
+          <pattern>
+            <patelt name="family">
+              <string>DejaVu Serif</string>
+            </patelt>
+          </pattern>
+          <pattern>
+            <patelt name="family">
+              <string>DejaVu Sans Mono</string>
+            </patelt>
+          </pattern>
 
-  #         <!-- Also reject EmojiOne Mozilla and Twemoji Mozilla; I want Noto Color Emoji -->
-  #         <pattern>
-  #           <patelt name="family">
-  #             <string>EmojiOne Mozilla</string>
-  #           </patelt>
-  #         </pattern>
-  #         <pattern>
-  #           <patelt name="family">
-  #             <string>Twemoji Mozilla</string>
-  #           </patelt>
-  #         </pattern>
-  #       </rejectfont>
-  #     </selectfont>
-  #   </fontconfig>
-  # '';
+          <!-- Also reject EmojiOne Mozilla and Twemoji Mozilla; I want Noto Color Emoji -->
+          <pattern>
+            <patelt name="family">
+              <string>EmojiOne Mozilla</string>
+            </patelt>
+          </pattern>
+          <pattern>
+            <patelt name="family">
+              <string>Twemoji Mozilla</string>
+            </patelt>
+          </pattern>
+        </rejectfont>
+      </selectfont>
+    </fontconfig>
+  '';
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
