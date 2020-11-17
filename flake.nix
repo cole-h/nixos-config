150247dc673ff34e53f0d32dc20e08b097bb8a80
Cole Helbling <cole.e.helbling@outlook.com>
2020-11-17 08:44:45 -0800

    scadrial: update

--- flake.nix
+++ flake.nix
@@ -99,7 +99,6 @@
               package = inputs.nix.defaultPackage.${system}.overrideAttrs ({ patches ? [ ], ... }: {
                 patches = patches ++ [
                   ./log-format-option.patch
-                  ./fix-ifd.patch # https://github.com/NixOS/nix/issues/4261, https://github.com/NixOS/nix/issues/4235
                 ];
               });
 
