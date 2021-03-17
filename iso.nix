4fd70b8c2444bbb4d7637e912a319cd248205713
Cole Helbling <cole.e.helbling@outlook.com>
2021-03-17 00:16:08 -0700

    cosmere: update iso config

--- iso.nix
+++ iso.nix
@@ -10,21 +10,11 @@
 
   nix.package = pkgs.nixUnstable;
 
-  programs.gnupg.agent = {
-    enable = true;
-    enableBrowserSocket = true;
-    enableExtraSocket = true;
-    enableSSHSupport = true;
-    pinentryFlavor = "curses";
-  };
-
   security.doas.enable = true;
 
   environment.systemPackages = with pkgs;
     [
       git
-      git-crypt
-      neovim
       kakoune
       htop
     ];
@@ -35,13 +25,13 @@
       { address = "192.168.1.69"; prefixLength = 24; }
     ];
     defaultGateway = "192.168.1.1";
-    nameservers = [ "192.168.1.212" "1.1.1.1" ];
+    nameservers = [ "192.168.1.212" "8.8.8.8" ];
   };
-
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6wCrUu1DHKFqeiRxNvIvv41rE5zS9rdingyKtZX5gy openpgp:0xF208643A"
-  ];
 
   systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
   services.sshd.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6wCrUu1DHKFqeiRxNvIvv41rE5zS9rdingyKtZX5gy openpgp:0xF208643A"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMcTaqUZSwv6YW8lx/JhsAZTdNSSC2fR8Pgk8woeFKh vin@scadrial"
+  ];
 }
