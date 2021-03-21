4629f34197d73d8a7ba3997c49b128db3a57c644
Cole Helbling <cole.e.helbling@outlook.com>
2021-03-21 00:04:10 -0700

    scadrial/libvirt: don't install libguestfs by default
    It wastes time building something I don't need it all the time; if I do,
    I'll deal with building it then.

--- hosts/scadrial/modules/libvirt/default.nix
+++ hosts/scadrial/modules/libvirt/default.nix
@@ -19,7 +19,7 @@ in
   environment.systemPackages = [
     # with-appliance or else `libguestfs: error: cannot find any suitable libguestfs supermin`
     # `doas guestmount -a /dev/rpool/win10 -m /dev/sda4 --ro /mnt`
-    pkgs.libguestfs-with-appliance
+    # pkgs.libguestfs-with-appliance
   ];
 
   users.users.vin.extraGroups = [
