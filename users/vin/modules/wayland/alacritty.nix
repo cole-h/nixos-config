9a3e963b805e38e6cf97272044a3ea7d143043f2
Cole Helbling <cole.e.helbling@outlook.com>
2021-03-21 20:19:04 -0700

    vin/wayland: remove alacritty colors

--- users/vin/modules/wayland/alacritty.nix
+++ users/vin/modules/wayland/alacritty.nix
@@ -43,41 +43,6 @@
         };
       };
 
-      # Dracula
-      colors = {
-        primary = {
-          background = "0x282A36";
-          foreground = "0xF8F8F2";
-        };
-
-        cursor = {
-          text = "0x44475A";
-          cursor = "0xF8F8F2";
-        };
-
-        normal = {
-          black = "0x282A36";
-          red = "0xFF5555";
-          green = "0x50FA7B";
-          yellow = "0xF1FA8C";
-          blue = "0xBD93F9";
-          magenta = "0xFF79C6";
-          cyan = "0x8BE9FD";
-          white = "0xF8F8F2";
-        };
-
-        bright = {
-          black = "0x4D4D4D";
-          red = "0xFF6E67";
-          green = "0x5AF78E";
-          yellow = "0xF4F99D";
-          blue = "0xCAA9FA";
-          magenta = "0xFF92D0";
-          cyan = "0x9AEDFE";
-          white = "0xE6E6E6";
-        };
-      };
-
       mouse.url.modifiers = "Shift";
 
       selection.save_to_clipboard = true;
