f712d1e632ec68b1a8d49452d0a1e70b8bf444b5
Cole Helbling <cole.e.helbling@outlook.com>
2021-05-10 13:20:59 -0700

    vin/mpd: configure spotifyd song notification

--- users/vin/modules/mpd.nix
+++ users/vin/modules/mpd.nix
@@ -1,5 +1,30 @@
 { super, config, lib, pkgs, ... }:
 
+let
+  spotifyd-change = pkgs.writeScript "spotifyd-change" ''
+    #!${pkgs.fish}/bin/fish
+
+    set meta (${pkgs.playerctl}/bin/playerctl -p spotifyd metadata | string join '\n')
+
+    set trackid (echo $meta | string split '\n' | ${pkgs.gawk}/bin/awk '/trackid\s/ {for(i=3;i<=NF;++i)printf $i FS}' | string trim)
+    test $trackid = (${pkgs.coreutils}/bin/cat /tmp/spotifyd-track) && exit
+    echo $trackid > /tmp/spotifyd-track
+
+    set title (echo $meta | string split '\n' | ${pkgs.gawk}/bin/awk '/title\s/ {for(i=3;i<=NF;++i)printf $i FS}' | string trim)
+    set artist (echo $meta | string split '\n' | ${pkgs.gawk}/bin/awk '!found && /artist\s/ {for(i=3;i<=NF;++i)printf $i FS; found=1}' | string trim)
+    set album (echo $meta | string split '\n' | ${pkgs.gawk}/bin/awk '/album\s/ {for(i=3;i<=NF;++i)printf $i FS}' | string trim)
+    set art (echo $meta | string split '\n' | ${pkgs.gawk}/bin/awk '/artUrl\s/ {for(i=3;i<=NF;++i)printf $i FS}' | string trim)
+
+    set artfile (${pkgs.coreutils}/bin/mktemp -t spotifyd.XXXXXXXXXX)
+    function finish --on-event fish_exit
+      ${pkgs.coreutils}/bin/rm -f $artfile
+    end
+
+    ${pkgs.curl}/bin/curl -ksSL "$art" -o "$artfile"
+
+    ${pkgs.libnotify}/bin/notify-send "$title" "$artist • $album" --icon="$artfile"
+  '';
+in
 {
   home.packages = with pkgs; [
     cantata # gui
@@ -21,6 +46,7 @@
       volume_normalisation = true;
       normalisation_pregain = -10;
       device_type = "speaker";
+      on_song_change_hook = spotifyd-change;
     };
   };
 
