4ca701700593007fe802ef75d7bb236c6aaa6899
Cole Helbling <cole.e.helbling@outlook.com>
2021-11-13 19:52:23 -0800

    scar/networking: make adguard available from :80

--- hosts/scar/modules/networking.nix
+++ hosts/scar/modules/networking.nix
@@ -23,9 +23,28 @@
 
   networking.firewall.allowedTCPPorts = [
     53 # adguardhome dns
+    80
   ];
 
   networking.firewall.allowedUDPPorts = [
     53 # adguardhome dns
   ];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts =
+      let
+        onlyLan = ''
+          allow 192.168.1.0/24;
+          allow 127.0.0.1;
+          deny all;
+        '';
+      in
+      {
+        "adguard.home".locations."/" = {
+          proxyPass = "http://127.0.0.1:3000/";
+          extraConfig = onlyLan;
+        };
+      };
+  };
 }
