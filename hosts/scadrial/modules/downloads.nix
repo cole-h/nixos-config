064f130a1f493863eda415c0f5e37487fde922a5
Cole Helbling <cole.e.helbling@outlook.com>
2021-11-20 16:34:02 -0800

    scadrial/downloads: clean up

--- hosts/scadrial/modules/downloads.nix
+++ hosts/scadrial/modules/downloads.nix
@@ -1,5 +1,4 @@
 { pkgs, config, ... }:
-# https://github.com/etu/nixconfig/blob/master/hosts/fenchurch/services/jellyfin.nix
 {
   users.groups.downloads.gid = 947;
   users.users.downloads = {
@@ -12,7 +11,6 @@
 
   users.users.vin.extraGroups = [ "downloads" ];
 
-  # http://localhost:9091/
   services.transmission = {
     enable = true;
     user = "downloads";
@@ -81,60 +79,4 @@
         };
       };
   };
-
-  ## Seems to take up too many resources (I hear crackling when listening to
-  ## music). Maybe the music's fault, but meh. TODO: revisit once on SSD (maybe
-  ## because it was on an HDD?)
-  # containers.downloads = {
-  #   autoStart = true;
-
-  #   config = { config, pkgs, ... }: {
-  #     users.users.downloads = { group = "downloads"; uid = 947; };
-  #     users.groups.downloads.gid = 947;
-
-  #     services.sonarr = {
-  #       enable = true;
-  #       user = "downloads";
-  #       group = "downloads";
-  #     };
-
-  #     services.transmission = {
-  #       enable = true;
-  #       user = "downloads";
-  #       group = "downloads";
-  #       home = "/var/lib/torrent";
-  #       settings = {
-  #         download-dir = "/var/lib/torrent/current";
-  #         rpc-whitelist = "127.0.0.1,192.168.*.*";
-  #         ratio-limit = "0";
-  #         ratio-limit-enabled = "true";
-  #         idle-seeding-limit = "1";
-  #         idle-seeding-limit-enabled = "true";
-  #       };
-  #     };
-  #   };
-
-  #   forwardPorts = [
-  #     { containerPort = 8989; hostPort = 8989; protocol = "tcp"; } # Sonarr
-  #     { containerPort = 9091; hostPort = 9091; protocol = "tcp"; } # Transmission
-  #   ];
-
-  #   bindMounts = {
-  #     "sonarr" = {
-  #       mountPoint = "/var/lib/sonarr";
-  #       hostPath = "/var/lib/sonarr";
-  #       isReadOnly = false;
-  #     };
-  #     "torrent" = {
-  #       mountPoint = "/var/lib/torrent";
-  #       hostPath = "/var/lib/torrent";
-  #       isReadOnly = false;
-  #     };
-  #     "media" = {
-  #       mountPoint = "/media";
-  #       hostPath = "/media";
-  #       isReadOnly = false;
-  #     };
-  #   };
-  # };
 }
