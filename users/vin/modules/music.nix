6e5d311b671ca13062dbc019ed612ce838c8bf8a
Cole Helbling <cole.e.helbling@outlook.com>
2021-12-26 11:45:20 -0800

    vin/music: hack around DAC suspend issue
    I got a FiiO K3 DAC/AMP. It likes to suspend when not playing audio,
    which results in an audible click. Whatever.

--- users/vin/modules/music.nix
+++ users/vin/modules/music.nix
@@ -121,4 +121,25 @@ in
       RestrictNamespaces = "yes";
     };
   };
+
+  # XXX: super duper ultra hack to make my FiiO K3 DAC not suspend when
+  # it's not playing audio...
+  systemd.user.services."fiio-k3-hack" = {
+    Install = {
+      WantedBy = [ "pipewire-pulse.service" ];
+    };
+    Service = {
+      ExecStart =
+        let
+          script = pkgs.writeShellScript "fiio-k3-hack" ''
+            ${pkgs.coreutils}/bin/sleep infinity | ${pkgs.pulseaudio}/bin/pacat -v
+          '';
+        in
+        toString script;
+    };
+    Unit = {
+      After = [ "pipewire-pulse.service" ];
+      Requires = [ "pipewire-pulse.service" ];
+    };
+  };
 }
