d2995927c5697373fbaa5d2ed7ac910624a81785
Cole Helbling <cole.e.helbling@outlook.com>
2022-01-30 22:19:39 -0800

    scar: make always accessible over tailscale

--- hosts/scar/modules/downloads/default.nix
+++ hosts/scar/modules/downloads/default.nix
@@ -32,4 +32,26 @@
     while ! ${pkgs.mullvad}/bin/mullvad status >/dev/null; do sleep 1; done
     ${pkgs.mullvad}/bin/mullvad lan set allow
   '';
+
+  networking.firewall.extraCommands =
+    let
+      # Allow all tailscale IPs to connect, in case something goes REALLY wrong
+      # and I can't access it over LAN.
+      ts = pkgs.writeText "tailscale.rules" ''
+        table inet excludeTraffic {
+          chain excludeOutgoing {
+            type route hook output priority -100; policy accept;
+            ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+
+          chain excludeIncoming {
+            type filter hook input priority -100; policy accept;
+            ip saddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+        }
+      '';
+    in
+    ''
+      ${pkgs.nftables}/bin/nft -f ${ts}
+    '';
 }
