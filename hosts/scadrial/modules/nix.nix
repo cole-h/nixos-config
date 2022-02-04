8ba445eb7c523b0fbe7ba235b9ce06bcb8a3df88
Cole Helbling <cole.e.helbling@outlook.com>
2022-02-03 16:03:18 -0800

    scadrial/nix: migrate to settings from raw options

--- hosts/scadrial/modules/nix.nix
+++ hosts/scadrial/modules/nix.nix
@@ -3,25 +3,25 @@
 {
   nix = {
     package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      builders-use-substitutes = true
-      secret-key-files = /root/cache-priv-key.pem
-    '';
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      builders-use-substitutes = true;
+      secret-key-files = "/root/cache-priv-key.pem";
 
-    trustedUsers = [ "vin" ];
-    binaryCaches = [
-      # "https://cache.qyliss.net"
-      # "https://cole-h.cachix.org"
-      # "https://nixpkgs-wayland.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
+      trusted-users = [ "vin" ];
+      substituters = [
+        # "https://cache.qyliss.net"
+        # "https://cole-h.cachix.org"
+        # "https://nixpkgs-wayland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
 
-    binaryCachePublicKeys = [
-      "qyliss-x220:bZQtoCyr68idLFb8UQeDjnjitO/xAj52gOo9GoKZuog="
-      "cole-h.cachix.org-1:qmEJ4uAe5tWwFxU/U5T/Nf2+wzXM3/rCP0SIGbK0dgU="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+      trusted-public-keys = [
+        "qyliss-x220:bZQtoCyr68idLFb8UQeDjnjitO/xAj52gOo9GoKZuog="
+        "cole-h.cachix.org-1:qmEJ4uAe5tWwFxU/U5T/Nf2+wzXM3/rCP0SIGbK0dgU="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 }
