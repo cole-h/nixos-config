b83e59887c15444d882a6ba84659ca2c33a51ec6
Cole Helbling <cole.e.helbling@outlook.com>
2022-07-28 11:16:30 -0700

    scar/wireguard: drop duckdns config
    I don't need it now that I have tailscale lol.

--- hosts/scar/modules/wireguard/default.nix
+++ hosts/scar/modules/wireguard/default.nix
@@ -18,10 +18,6 @@
     wg0-psk = {
       file = ./psk;
     };
-
-    duckdns = {
-      file = ./duckdns;
-    };
   };
 
   networking.wireguard.interfaces = {
@@ -56,22 +52,7 @@
           name = "wireguard-${k}";
           value = { serviceConfig.SupplementaryGroups = [ config.users.groups.keys.name ]; };
         })
-        (builtins.attrNames config.networking.wireguard.interfaces))
-    // {
-      update-duckdns = {
-        serviceConfig = {
-          Type = "oneshot";
-          SupplementaryGroups = [ config.users.groups.keys.name ];
-        };
-        # This is just so the secret doesn't show up in my (public) repo -- if
-        # somebody has local access to my system, them being able to update my
-        # DuckDNS IP will be the least of my worries.
-        script = ''
-          echo url="https://www.duckdns.org/update?domains=scadrial&token=$(cat ${config.age.secrets.duckdns.path})" \
-            | ${pkgs.curl}/bin/curl -K - 2>/dev/null
-        '';
-      };
-    };
+        (builtins.attrNames config.networking.wireguard.interfaces));
 
   systemd.timers.update-duckdns = {
     wantedBy = [ "timers.target" ];
