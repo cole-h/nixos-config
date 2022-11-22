6f842c262d9377823ac9beb9e7c713b4e103715d
Cole Helbling <cole.e.helbling@outlook.com>
2022-11-21 19:12:30 -0800

    scar: remove defunct timer

--- hosts/scar/modules/wireguard/default.nix
+++ hosts/scar/modules/wireguard/default.nix
@@ -53,14 +53,4 @@
           value = { serviceConfig.SupplementaryGroups = [ config.users.groups.keys.name ]; };
         })
         (builtins.attrNames config.networking.wireguard.interfaces));
-
-  systemd.timers.update-duckdns = {
-    wantedBy = [ "timers.target" ];
-    after = [ "home-manager-vin.service" "network.target" ];
-    timerConfig = {
-      Unit = "update-duckdns.service";
-      OnCalendar = "hourly";
-      Persistent = true;
-    };
-  };
 }
