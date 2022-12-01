7bf1e1dc9c6d13fdda9ebf9a0e977b10e36c4793
Cole Helbling <cole.e.helbling@outlook.com>
2022-11-30 16:13:35 -0800

    vin/fish: add humantime function

--- users/vin/modules/fish.nix
+++ users/vin/modules/fish.nix
@@ -17,6 +17,26 @@ in
         win10 = "doas virsh start windows10";
         fish_greeting = "";
         fish_user_key_bindings = "bind \\cw backward-kill-word";
+
+        # https://github.com/jorgebucaran/humantime.fish/blob/53b2adb4c6aff0da569c931a3cc006efcd0e7219/functions/humantime.fish
+        # TODO: run this on $CMD_DURATION in prompt
+        humantime = {
+          argumentNames = [ "ms" ];
+          description = "Turn milliseconds into a human-readable string";
+          body = ''
+            set --query ms[1] || return
+
+            set --local secs (math --scale=1 $ms/1000 % 60)
+            set --local mins (math --scale=0 $ms/60000 % 60)
+            set --local hours (math --scale=0 $ms/3600000)
+
+            test $hours -gt 0 && set --local --append out $hours"h"
+            test $mins -gt 0 && set --local --append out $mins"m"
+            test $secs -gt 0 && set --local --append out $secs"s"
+
+            set --query out && echo $out || echo $ms"ms"
+          '';
+        };
       };
 
       shellAbbrs = {
