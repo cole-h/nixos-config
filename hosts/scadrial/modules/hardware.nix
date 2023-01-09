ca90863359ea50a2c3232c818625dfbfb3a9c410
Cole Helbling <cole.e.helbling@outlook.com>
2023-01-09 09:16:23 -0800

    scadrial/hardware: fix front panel audio

--- hosts/scadrial/modules/hardware.nix
+++ hosts/scadrial/modules/hardware.nix
@@ -27,4 +27,31 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
   boot.kernelParams = [ "module_blacklist=i915" ];
+
+  system.replaceRuntimeDependencies = [
+    {
+      original = pkgs.alsa-ucm-conf;
+      replacement = pkgs.alsa-ucm-conf.overrideAttrs ({ patches ? [ ], ... }: {
+        patches = patches ++ [
+          # https://github.com/alsa-project/alsa-ucm-conf/pull/267
+          (pkgs.writeText "support-my-mobo.patch" ''
+            diff --git a/ucm2/USB-Audio/USB-Audio.conf b/ucm2/USB-Audio/USB-Audio.conf
+            index 325d48c..a028eb1 100644
+            --- a/ucm2/USB-Audio/USB-Audio.conf
+            +++ b/ucm2/USB-Audio/USB-Audio.conf
+            @@ -41,7 +41,8 @@ If.realtek-alc4080 {
+             		# 0db0:1feb MSI Edge Wifi Z690
+             		# 0db0:419c MSI MPG X570S Carbon Max Wifi
+             		# 0db0:a073 MSI MAG X570S Torpedo Max
+            -		Regex "USB((0b05:(1996|1a2[07]))|(0db0:(1feb|419c|a073)))"
+            +		# 0db0:6c09 MSI MPG Z790 Cargon Wifi
+            +		Regex "USB((0b05:(1996|1a2[07]))|(0db0:(1feb|419c|a073|6c09)))"
+             	}
+             	True.Define.ProfileName "Realtek/ALC4080"
+             }
+          '')
+        ];
+      });
+    }
+  ];
 }
