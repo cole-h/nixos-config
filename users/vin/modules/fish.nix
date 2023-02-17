f3e555b9947981a8302bf98286e817d0b4e9ff79
Cole Helbling <cole.e.helbling@outlook.com>
2023-02-17 11:08:20 -0800

    vin/fish: add command time to prompt

--- users/vin/modules/fish.nix
+++ users/vin/modules/fish.nix
@@ -19,7 +19,6 @@ in
         fish_user_key_bindings = "bind \\cw backward-kill-word";
 
         # https://github.com/jorgebucaran/humantime.fish/blob/53b2adb4c6aff0da569c931a3cc006efcd0e7219/functions/humantime.fish
-        # TODO: run this on $CMD_DURATION in prompt
         humantime = {
           argumentNames = [ "ms" ];
           description = "Turn milliseconds into a human-readable string";
@@ -34,7 +33,47 @@ in
             test $mins -gt 0 && set --local --append out $mins"m"
             test $secs -gt 0 && set --local --append out $secs"s"
 
-            set --query out && echo $out || echo $ms"ms"
+            if set --query out
+                string replace --all " " "" $out || :
+            else
+                echo $ms"ms"
+            end
+          '';
+        };
+
+        fish_prompt = {
+          description = "Write out the prompt";
+          body = ''
+              set -l last_pipestatus $pipestatus
+              set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+              set -l normal (set_color normal)
+              set -q fish_color_status
+              or set -g fish_color_status red
+
+              # Color the prompt differently when we're root
+              set -l color_cwd $fish_color_cwd
+              set -l suffix '>'
+              if functions -q fish_is_root_user; and fish_is_root_user
+                  if set -q fish_color_cwd_root
+                      set color_cwd $fish_color_cwd_root
+                  end
+                  set suffix '#'
+              end
+
+              # Write pipestatus
+              # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+              set -l bold_flag --bold
+              set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+              if test $__fish_prompt_status_generation = $status_generation
+                  set bold_flag
+              end
+              set __fish_prompt_status_generation $status_generation
+              set -l status_color (set_color $fish_color_status)
+              set -l statusb_color (set_color $bold_flag $fish_color_status)
+              set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+              set -l last_command_time (humantime $CMD_DURATION)
+
+              echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status " "$last_command_time $suffix " "
           '';
         };
       };
