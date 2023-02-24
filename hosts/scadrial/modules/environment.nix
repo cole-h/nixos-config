b0274844f28953a124beef37e0703c7291b75790
Cole Helbling <cole.e.helbling@outlook.com>
2023-02-24 08:26:07 -0800

    scadrial/environment: clean up packages list

--- hosts/scadrial/modules/environment.nix
+++ hosts/scadrial/modules/environment.nix
@@ -7,22 +7,15 @@
     [
       agenix # secrets
       age # secrets
-      bat # cat but better
       binutils # ld, strip
-      # borgbackup
       bottom # fancy top
-      # cachix
-      # cntr # used for breakpointHook
-      # cryptsetup # for borgbackup
-      dnsutils
-      # dogdns # dig but better # broken build atm
+      dnsutils # dig, nslookup
       efibootmgr
       evince # pdf viewer
       exa # ls but better
       fd # find files
       ffmpeg # video conversion and stuff
       file # check file types
-      # filezilla # for switch
       gcc # compilation, but also for rust
       gdb # debugging
       gitFull # ...git
@@ -30,30 +23,27 @@
       gnome.nautilus # GUI file manager
       hexyl # hex viewer
       htop # top but better
-      imagemagick # cli image manipulation
       libqalculate # greatest cli calculator ever, with conversions too
-      # libreoffice # Office but worse
       libressl.nc # netcat-openbsd
       man-pages # man-pages stuff
       man-pages-posix # posix man pages
+      musl.dev # for musl-gcc, static compilation of rust programs
       ncdu # friendlier du
       nix-index # nix-locate
       nixpkgs-fmt # the better formatter
       nix-top # see what's building
-      # openconnect # for school VPN, if needed
       openssl # playing with tls and more
       par # nice paragraph formatter
       pavucontrol # volume gui
       pciutils # lspci, etc
       psmisc # ps
-      ripgrep # grep but better; [overlays]
+      ripgrep # grep but better
       rsync # send files over ssh
       strace # trace syscalls and stuff
-      # tmate # "Instant Terminal Sharing"
       tokei # code metrics
       usbutils # lsusb
       wireguard-tools # wg, etc.
       xdg-utils # xdg-mime, xdg-open
-      # zathura # fails to build: https://github.com/NixOS/nixpkgs/issues/187305
+      zathura # minimal pdf viewer
     ];
 }
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -11,7 +11,6 @@ in
   wezterm = callPackage ./drvs/wezterm { };
 
   # small-ish overrides
-  ripgrep = prev.ripgrep.override { withPCRE2 = true; };
   rofi = prev.rofi.override { plugins = [ final.rofi-emoji ]; };
   bash-preexec = prev.bash-preexec.overrideAttrs ({ ... }: { doCheck = false; });
 
