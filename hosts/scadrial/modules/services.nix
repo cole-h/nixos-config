00cf73867f6cb1be17b210b97ebec955a6b5606f
Cole Helbling <cole.e.helbling@outlook.com>
2023-04-12 10:06:08 -0700

    scadrial/services: enable postgres

--- hosts/scadrial/modules/services.nix
+++ hosts/scadrial/modules/services.nix
@@ -78,4 +78,12 @@
   services.nscd.enableNsncd = true;
 
   services.safeeyes.enable = true;
+
+  services.postgresql.enable = true;
+  services.postgresql.package = pkgs.postgresql_15;
+  services.postgresql.authentication = ''
+    local all all trust
+    host all all 127.0.0.1/32 trust
+    host all all ::1/128 trust
+  '';
 }
