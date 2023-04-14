80177d3fddca28c76102db37708a76b2fff7bc74
Cole Helbling <cole.e.helbling@outlook.com>
2023-04-14 09:12:32 -0700

    vin/fish: drop unnecessary functions

--- users/vin/modules/fish.nix
+++ users/vin/modules/fish.nix
@@ -13,8 +13,6 @@ in
         nix-locate = "command nix-locate --top-level $argv";
         ssh = "env TERM=xterm-256color ssh $argv";
         # std = "rustup doc --std";
-        t = "todo.sh $argv";
-        win10 = "doas virsh start windows10";
         fish_greeting = "";
         fish_user_key_bindings = "bind \\cw backward-kill-word";
 
