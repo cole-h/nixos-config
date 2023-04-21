576a1b07d81863e7596c430cabf24c43c81acbd6
Cole Helbling <cole.e.helbling@outlook.com>
2023-04-21 10:22:07 -0700

    modules/nix: enable repl-flake feature

--- modules/config/nix.nix
+++ modules/config/nix.nix
@@ -4,7 +4,7 @@
     nixPath = [ ];
     distributedBuilds = true; # necessary for settings.builders to not be defined in the nix-daemon upstream module
     settings = {
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       flake-registry = "/etc/nix/registry.json";
       builders = [ "@/etc/nix/machines" ];
       trusted-public-keys = [
