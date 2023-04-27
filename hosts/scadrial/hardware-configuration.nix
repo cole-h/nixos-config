f1a753e58a94f12b09c29734694c8967c379fb86
Cole Helbling <cole.e.helbling@outlook.com>
2023-04-27 11:00:37 -0700

    scadrial/hardware-configuration: update kernelModules
    Forgot this when I updated my hardware oops.

--- hosts/scadrial/hardware-configuration.nix
+++ hosts/scadrial/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
