a722b9e96931d7c64a7c0bd711f62b45a44527c5
Cole Helbling <cole.e.helbling@outlook.com>
2023-06-02 06:33:33 -0700

    flake: expand, format inputs

--- flake.nix
+++ flake.nix
@@ -11,15 +11,33 @@
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
 
-    agenix-cli = { url = "github:cole-h/agenix-cli"; inputs.nixpkgs.follows = "nixpkgs"; };
-    agenix = { url = "github:ryantm/agenix"; inputs.nixpkgs.follows = "nixpkgs"; };
-    home = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nix = { url = "github:nixos/nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    agenix-cli = {
+      url = "github:cole-h/agenix-cli";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix = {
+      url = "github:nixos/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # nix = { url = "github:edolstra/nix/lazy-trees"; inputs.nixpkgs.follows = "nixpkgs"; };
-    naersk= { url = "github:nix-community/naersk"; inputs.nixpkgs.follows = "nixpkgs"; };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Not flakes
-    wezterm = { url = "git+https://github.com/wez/wezterm.git?submodules=1"; flake = false; };
+    wezterm = {
+      url = "git+https://github.com/wez/wezterm.git?submodules=1";
+      flake = false;
+    };
   };
 
   outputs = inputs:
