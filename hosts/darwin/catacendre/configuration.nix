37c6f98274a0a785ffbccd304527151119e9a7d5
Cole Helbling <cole.e.helbling@outlook.com>
2023-06-06 16:19:34 -0700

    catacendre: don't verify build users

--- hosts/darwin/catacendre/configuration.nix
+++ hosts/darwin/catacendre/configuration.nix
@@ -19,4 +19,8 @@
   # We install Nix using a separate installer so we don't want nix-darwin
   # to manage it for us. This tells nix-darwin to just use whatever is running.
   nix.useDaemon = true;
+
+  # We use the auto-allocated-uids Nix feature, so there are no build users to
+  # validate (Nix will create them as needed).
+  system.checks.verifyBuildUsers = false;
 }
