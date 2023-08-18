3da4c9ffe18cfbd1bc1873bc6c563121cee459b1
Cole Helbling <cole.e.helbling@outlook.com>
2023-08-18 14:32:47 -0700

    flake.lock: use fixed naersk
    Flake lock file updates:
    
    • Updated input 'naersk':
        'github:nix-community/naersk/d9a33d69a9c421d64c8d925428864e93be895dcc' (2023-07-26)
      → 'github:nix-community/naersk/78789c30d64dea2396c9da516bbcc8db3a475207' (2023-08-18)

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     };
     # nix = { url = "github:edolstra/nix/lazy-trees"; inputs.nixpkgs.follows = "nixpkgs"; };
     naersk = {
-      url = "github:nix-community/naersk/d9a33d69a9c421d64c8d925428864e93be895dcc";
+      url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
