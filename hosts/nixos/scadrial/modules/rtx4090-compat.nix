27e35f79a7e892932efdb41ad84343ab801d0cd9
Cole Helbling <cole.e.helbling@outlook.com>
2024-01-31 15:33:43 -0800

    scadrial/4090: use zfs 2.2.3 (not yet released) and kernel 6.7
    Look ma, no* patches!
    
    * OK, I override ZFS to use 2.2.3 which isn't yet released, but
    whatever. Surely nothing bad will happen.
    
    Huge shoutout to Ben Skeggs (formerly RedHat) for making this possible.

--- hosts/nixos/scadrial/modules/rtx4090-compat.nix
+++ hosts/nixos/scadrial/modules/rtx4090-compat.nix
@@ -1,77 +1,41 @@
 { lib, pkgs, ... }:
+let
+  version = "2.2.3-unstable-2024-01-30";
+  src = pkgs.fetchFromGitHub {
+    owner = "openzfs";
+    repo = "zfs";
+    rev = "eeab554b8a07363bfe8f9a064811d06219f4f0f6";
+    hash = "sha256-s9xAP+RJDJdyu9jAS2OkogS1FR9VRd70yQCBJpPO5iw=";
+  };
+in
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      zfs = prev.zfs.overrideAttrs ({ ... }: {
+        name = "zfs-user-${version}";
+        inherit src;
+      });
+    })
+  ];
+
   # FIXME: once linux 6.7 is released and in nixpkgs, AND ZFS supports 6.7 kernels
   # https://github.com/NixOS/nixpkgs/pull/271703
   # https://github.com/openzfs/zfs/issues/15582
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelPackages =
     let
-      kernelPackage = pkgs.linuxKernel.packages.linux_6_1.kernel;
+      kernelPackage = pkgs.linuxKernel.packages.linux_6_7.kernel;
+      linuxPackages = (pkgs.linuxPackagesFor kernelPackage).extend
+        (final: prev: {
+          zfs = prev.zfs.overrideAttrs ({ ... }: {
+            name = "zfs-kernel-${version}-${final.kernel.version}";
+            inherit src;
+            meta.broken = false;
+          });
+        });
     in
-    lib.mkForce
-      (pkgs.linuxPackagesFor
-        (kernelPackage.override {
-          kernelPatches =
-            lib.subtractLists
-              [
-                # This Dell XPS patch doesn't apply to 6.1.23
-                # Caused by: https://github.com/NixOS/nixpkgs/pull/255824
-                pkgs.linuxKernel.kernelPatches.dell_xps_regression
-              ]
-              kernelPackage.kernelPatches;
-
-          # X86_PLATFORM_DRIVERS_HP option is unused on 6.1.23
-          # Caused by: https://github.com/NixOS/nixpkgs/pull/255846
-          structuredExtraConfig = {
-            X86_PLATFORM_DRIVERS_HP = lib.mkForce lib.kernel.unset;
-          };
-
-          argsOverride = rec {
-            version = "6.1.23";
-            modDirVersion = version;
-            src = pkgs.fetchurl {
-              url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-              sha256 = "sha256-dFg3LodQr+N/0aw+erPCLyxgGPdg+BNAVaA/VKuj6+s=";
-            };
-          };
-        }));
+    lib.mkForce (pkgs.recurseIntoAttrs linuxPackages);
 
   boot.kernelParams = [
     "module_blacklist=i915"
-  ];
-
-  boot.kernelPatches = [
-    {
-      name = "nouveau.patch";
-      patch = ./nouveau.patch;
-    }
-  ];
-
-  hardware.firmware = [
-    (pkgs.stdenv.mkDerivation {
-      pname = "nvidia-gsp-firmware";
-      version = "525.60.11";
-      # FIXME: will be 535.113.01 in linux 6.7 -- grep the linux source for `ad102_gsps` to find the version in case it changes
-      # https://elixir.bootlin.com/linux/latest/A/ident/ad102_gsps ("as a variable")
-      # Currently defined in: drivers/gpu/drm/nouveau/nvkm/subdev/gsp/ad102.c
-      src = pkgs.fetchurl {
-        url = "https://us.download.nvidia.com/XFree86/Linux-x86_64/525.60.11/NVIDIA-Linux-x86_64-525.60.11.run";
-        sha256 = "sha256-gW7mwuCBPMw9SnlY9x/EmjfGDv4dUdYUbBznJAOYPV0=";
-      };
-
-      dontBuild = true;
-      dontFixup = true;
-      dontStrip = true;
-
-      unpackPhase = ''
-        sh $src --extract-only --target build
-        cd build
-      '';
-
-      installPhase = ''
-        mkdir -p $out/lib/firmware/nvidia/ad102/gsp
-        mv firmware/gsp_ad10x.bin $out/lib/firmware/nvidia/ad102/gsp/gsp-5256011.bin
-      '';
-    })
   ];
 }
