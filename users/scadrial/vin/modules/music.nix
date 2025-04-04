ba67fea9b42eea6a29350d3330f0626030c8f511
Cole Helbling <cole.e.helbling@outlook.com>
2025-04-04 14:59:54 -0700

    vin/music: get rid of fiio-k3-hack service
    I now use a MOTU M2 as my interface, which doesn't have this problem :)

--- users/scadrial/vin/modules/music.nix
+++ users/scadrial/vin/modules/music.nix
@@ -4,26 +4,4 @@
   home.packages = with pkgs; [
     spotify
   ];
-
-  # XXX: super duper ultra hack to make my FiiO K3 DAC not suspend when
-  # it's not playing audio...
-  systemd.user.services."fiio-k3-hack" = {
-    Install = {
-      WantedBy = [ "pipewire-pulse.service" ];
-    };
-    Service = {
-      ExecStart =
-        let
-          script = pkgs.writeShellScript "fiio-k3-hack" ''
-            while ! echo | ${pkgs.pulseaudio}/bin/pacat -v; do sleep 1; done
-            ${pkgs.coreutils}/bin/sleep infinity | ${pkgs.pulseaudio}/bin/pacat -v
-          '';
-        in
-        toString script;
-    };
-    Unit = {
-      After = [ "pipewire-pulse.service" "sway-session.target" ];
-      Requires = [ "pipewire-pulse.service" ];
-    };
-  };
 }
