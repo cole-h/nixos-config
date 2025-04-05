92d8ff1ab999fb085cd7e280e2ea1edb1587ba6a
Cole Helbling <cole.e.helbling@outlook.com>
2025-04-05 12:28:21 -0700

    vin/sway: remove adaptive_sync config
    This hasn't actually done anything:
    
        $ swaymsg -t get_outputs
        Output DP-3 'Dell Inc. DELL S2721DGF CPYJZN3' (focused)
          [...snip...]
          Adaptive sync: disabled

--- users/scadrial/vin/modules/wayland/sway.nix
+++ users/scadrial/vin/modules/wayland/sway.nix
@@ -104,7 +104,6 @@ in
           scale = "1";
         };
         "DP-3" = {
-          adaptive_sync = "on";
           resolution = "2560x1440@165Hz";
           position = "1920,0";
           scale = "1";
