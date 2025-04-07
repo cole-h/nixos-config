75739de22fa034de2da4ecb401df259d9e86ce31
Cole Helbling <cole.e.helbling@outlook.com>
2025-04-06 17:16:14 -0700

    scadrial/programs: disable `info` tool, remove adb

--- hosts/nixos/scadrial/modules/programs.nix
+++ hosts/nixos/scadrial/modules/programs.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 
 {
+  documentation.info.enable = false;
   documentation.dev.enable = true;
   documentation.man.enable = true;
   documentation.man.generateCaches = false;
@@ -14,12 +15,6 @@
   programs.fish.enable = true;
 
   programs.command-not-found.enable = false;
-
-  # 0. Plug in phone and make sure it's detected
-  # 1. adb tcpip 5555
-  # 2. Disconnect phone
-  # 3. adb connect [ip]
-  programs.adb.enable = true;
 
   programs.iotop.enable = true;
 
