783c1040f0e3d89497812438d6db9b8fec899837
Cole Helbling <cole.e.helbling@outlook.com>
2025-04-10 07:22:57 -0700

    scadrial/4090: switch to the nvidia drivers

--- hosts/nixos/scadrial/modules/rtx4090-compat.nix
+++ hosts/nixos/scadrial/modules/rtx4090-compat.nix
@@ -1,6 +1,14 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   boot.kernelParams = [
     "module_blacklist=i915"
   ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = false;
+  hardware.nvidia.powerManagement.finegrained = false;
+  hardware.nvidia.open = true;
+  hardware.nvidia.nvidiaSettings = true;
 }
