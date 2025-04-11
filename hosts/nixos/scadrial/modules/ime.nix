29bca8deb82e9558c13e09de743d32bc851f1b39
Cole Helbling <cole.e.helbling@outlook.com>
2025-04-10 18:59:37 -0700

    scadrial/ime: cleanup, enable wayland frontend

--- hosts/nixos/scadrial/modules/ime.nix
+++ hosts/nixos/scadrial/modules/ime.nix
@@ -1,20 +1,11 @@
 { pkgs, ... }:
 {
-  i18n = {
-    # https://github.com/cideM/dotfiles/blob/7c4b1d589c5263e3884af2b2a0f8f51cddc650d7/hosts/nixos/configuration.nix#L72-L79
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-mozc
-        fcitx5-gtk
-      ];
-    };
-
-    # https://discourse.nixos.org/t/wayland-and-fcitx5-japanese-input/11488/4
-    # inputMethod = {
-    #   enabled = "ibus";
-    #   ibus.engines = with pkgs.ibus-engines; [ mozc ];
-    # };
-  };
+  # https://github.com/cideM/dotfiles/blob/7c4b1d589c5263e3884af2b2a0f8f51cddc650d7/hosts/nixos/configuration.nix#L72-L79
+  i18n.inputMethod.enable = true;
+  i18n.inputMethod.type = "fcitx5";
+  i18n.inputMethod.fcitx5.waylandFrontend = true;
+  i18n.inputMethod.fcitx5.addons = with pkgs; [
+    fcitx5-mozc
+    fcitx5-gtk
+  ];
 }
