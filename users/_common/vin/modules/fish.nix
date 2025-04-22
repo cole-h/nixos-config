671db21a689fb9975855366cb7941c04485afe4b
Cole Helbling <cole.e.helbling@outlook.com>
2025-04-22 11:24:25 -0700

    vin/fish: add fish_jj_prompt
    I've been using it locally but forgot to actually commit it lol.

--- users/_common/vin/modules/fish.nix
+++ users/_common/vin/modules/fish.nix
@@ -79,6 +79,44 @@ in
           '';
         };
 
+        fish_jj_prompt = {
+          description = "Write out the jj prompt";
+          body = ''
+            # Is jj installed?
+            if not command -sq jj
+                return 1
+            end
+
+            # Are we in a jj repo?
+            if not jj root --ignore-working-copy --quiet &>/dev/null
+                return 1
+            end
+
+            # Generate prompt
+            set -l info "$(
+                set -l op_id (jj op log --ignore-working-copy --no-graph --limit=1 --template 'self.id().short()')
+                jj log --ignore-working-copy --no-graph --color=always --revisions=@ --template "
+                    separate(
+                        ' ',
+                        label('operation id', '$op_id'),
+                        bookmarks.join(', '),
+                        if(conflict, label('conflict', '×')),
+                        if(empty, label('empty', '(empty)')),
+                        if(divergent, label('divergent', '(divergent)')),
+                        if(hidden, label('hidden', '(hidden)')),
+                    )
+                "
+            )"
+            or return 1
+
+            if test -n $info
+                printf ' (%s)' $info
+            else
+                return 1
+            end
+          '';
+        };
+
         fish_prompt = {
           description = "Write out the prompt";
           body = ''
