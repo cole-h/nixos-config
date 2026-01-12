197531f4ba66667a4f2dcb5496d383b5a29cfddf
Cole Helbling <cole.e.helbling@outlook.com>
2026-01-12 10:12:33 -0800

    nixos/boot: Linux 6.18

--- hosts/nixos/_modules/config/boot.nix
+++ hosts/nixos/_modules/config/boot.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 {
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_6_17;
+    # NOTE(cole-h): Do _not_ let this slide back to a version before 6.7/6.8 -- scadrial will not
+    # get graphics with its 4090...
+    kernelPackages = pkgs.linuxKernel.packages.linux_6_18;
     supportedFilesystems = [ "zfs" ];
     zfs.package = pkgs.zfs_unstable;
   };
